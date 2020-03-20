1888487b958a8a6bf616a06f9cb6c8307dc433a6
Ryan Knight <rgknight@users.noreply.github.com>
2020-03-20 17:00:02 -0400

    Updates

---
+++ precache-manifest.bf2725bf7943f629ca8f83d1c3838bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efaf727f853343a9c666c47c1cb05a1f",
+    "url": "/covid-19-front/index.html"
+  },
+  {
+    "revision": "7f518e0e37b98821ccd9",
+    "url": "/covid-19-front/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "3503d2bffad00ea71efd",
+    "url": "/covid-19-front/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f518e0e37b98821ccd9",
+    "url": "/covid-19-front/static/js/2.242db9f0.chunk.js"
+  },
+  {
+    "revision": "76a55b301db41b8b467250603e7d866f",
+    "url": "/covid-19-front/static/js/2.242db9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3503d2bffad00ea71efd",
+    "url": "/covid-19-front/static/js/main.98abb0b2.chunk.js"
+  },
+  {
+    "revision": "1450874d358ad09fba38",
+    "url": "/covid-19-front/static/js/runtime-main.f5c424a8.js"
+  }
+]);
